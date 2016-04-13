d05188c6d8b077d0b21c690dbceaaa50ed975d48
Daniel Campbell <metalwhirlwind@hotmail.com>
2016-04-12 20:34:26 -0700

    Initial Commit

---
+++ ShareCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShareCalc
+//
+//  Created by Daniel Campbell on 4/12/16.
+//  Copyright © 2016 Daniel Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
